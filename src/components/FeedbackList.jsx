431939a7aa9400a19b7a91f1276e7c81702b1ba8
chengen-sung <chengen.sung@sjsu.edu>
2022-08-19 19:49:34 -0700

    [Feedback UI] Add animation for feedacks using framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,33 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
+import { motion, AnimatePresence } from "framer-motion";
+
 const FeedbackList = ({ feedbacks, onFeedbackDelete }) => {
-    return (feedbacks != null && feedbacks.length > 0) ? (
-        <div className='feedback-list'>
-            {feedbacks.map( (feedback) => (
-                <FeedbackItem key={feedback.id} feedback={feedback} onFeedbackDelete={onFeedbackDelete}/>
-            ))}
+    return feedbacks != null && feedbacks.length > 0 ? (
+        <div className="feedback-list">
+            <AnimatePresence>
+                {feedbacks.map((feedback) => (
+                    <motion.div
+                        key = {feedback.id}
+                        initial={{ opacity: 0, scale: 0.95 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0}}
+                        transition={{ duration: 0.4 }}
+                    >
+                        <FeedbackItem
+                            key={feedback.id}
+                            feedback={feedback}
+                            onFeedbackDelete={onFeedbackDelete}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     ) : (
         <p> No Feedback Yet</p>
-    )
+    );
 };
 
 FeedbackList.propTypes = {
